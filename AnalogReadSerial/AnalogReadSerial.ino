e29d7d58bb0f767e2270ff09522400bdfa12c410
Anna Anderson <anan6107@colorado.edu>
2020-03-03 09:37:06 -0700

    Starting to reformat for readability

--- AnalogReadSerial/AnalogReadSerial.ino
+++ AnalogReadSerial/AnalogReadSerial.ino
@@ -1,4 +1,10 @@
 char incoming;
+char a = 's';
+
+
+// Serial.print(toyou);
+// Serial.write(tome);
+
 
 void setup() {
   // initialize serial communication at 9600 bits per second:
@@ -7,11 +13,11 @@ void setup() {
 
 // the loop routine runs over and over again forever:
 void loop() {
-  
-  if (Serial.available() > 0){
+   delay(600);
+   Serial.write (a);
+  while (Serial.available() > 0){
     incoming = Serial.read();
-    Serial.println(incoming);
+    Serial.write(incoming);
+//    Serial.print(incoming);
   }
-//  Serial.write("hi");
-  
 }
